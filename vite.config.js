263d9d822797c465791681f6bb06e400acaca0f4
Sujju2468 <93813696+Sujju2468@users.noreply.github.com>
2025-04-05 19:04:28 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0', // Make the server listen on all available network interfaces
+    host: '127.0.0.1', // Make the server listen on all available network interfaces
     port: 5173, // Optional: specify the port number (defaults to 3000)
   },
 })
