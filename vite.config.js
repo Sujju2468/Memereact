a35ae51f05489a553493299684760761698fb68a
Sujju2468 <93813696+Sujju2468@users.noreply.github.com>
2025-04-05 18:21:50 +0530

    Update vite.config.js for public access

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // Make the server listen on all available network interfaces
+    port: 5173, // Optional: specify the port number (defaults to 3000)
+  },
 })
